711230e60c5e71cbf163f8a4bb24f4201c2bb187
CerFrank <37081877+CerFrank@users.noreply.github.com>
2020-07-20 19:14:30 +0200

    Implements EF (incomplete)

---
+++ BillionSort/BillionSort/Migrations/20200720170111_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BillionSort.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    CustomData = table.Column<string>(nullable: true),
+                    CustomData02 = table.Column<string>(nullable: true),
+                    CustomData03 = table.Column<string>(nullable: true),
+                    CustomData04 = table.Column<string>(nullable: true),
+                    CustomData05 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
---
+++ BillionSort/BillionSort/Models/AccountsContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BillionSort.Models
+{
+    public class AccountsContext : DbContext
+    {
+        DbSet<Account> Accounts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=INLL9DL-LT\\MSSQLSERVER01;Initial Catalog=Accounts;Integrated Security=True");
+    }
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string CustomData { get; set; }
+        public string CustomData02 { get; set; }
+        public string CustomData03 { get; set; }
+        public string CustomData04 { get; set; }
+        public string CustomData05 { get; set; }
+
+    }
+
+}
